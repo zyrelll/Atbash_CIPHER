3b315d8ddd73f2a6a9aa109f308cb8e226b384f3
Arif Suryo Kurniawan <jayadewatabagaskara@gmail.com>
2023-11-13 20:55:44 +0700

    ver 1.1

--- README.md
+++ README.md
@@ -9,3 +9,20 @@ the Hebrew alphabet is “bet”, which gets
 encrypted to the second-last letter, “shin”.
 Putting these four letters together gives
 something which sounds like “atbash”.
+
+# Alphabet
+
+The alphabet for Modern English is a Latin-script alphabet consisting of 26 letters, each having an upper- and lower-case form. The word alphabet is a compound of the first two letters of the Greek alphabet, alpha and beta. The alphabet originated around the 7th century to write Old English from Latin script. Since then, letters have been added or removed to give the current letters:
+
+A aB bC cD dE eF fG gH hI iJ jK kL lM mN nO oP pQ qR rS sT tU uV vW wX xY yZ z
+The exact shape of printed letters varies depending on the typeface (and font), and the standard printed form may differ significantly from the shape of handwritten letters (which varies between individuals), especially cursive.
+
+The English alphabet has 5 vowels, 19 consonants, and 2 letters (Y and W) that can function as consonants or vowels.
+
+Written English has a large number of digraphs, such as ch, ea, oo, sh, and th. Within the languages used in Europe, English stands out in not normally using diacritics in native words.
+
+# Version
+ver 1.0: basic use of atbash cipher with modern english alphabet.
+ver 1.1: add inputMessage() function, add uppercase alphabet capability
+(ongoing)
+ver 1.2: add other alphabet version
--- atbash.py
+++ atbash.py
@@ -1,18 +1,29 @@
 def decrypt(message, matrix):
     decrypted_message = ""
     for char in message:
-        if char == " ":
+        if char == " ": 
             decrypted_message += " "
         else:
-            index = matrix[1].index(char)
-            decrypted_message += matrix[0][index]
+            index = matrix[2 if char.islower() else 3].index(char)
+            decrypted_message += matrix[0 if char.islower() else 1][index]
     return decrypted_message
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-reversed_alphabet = ''.join(reversed(alphabet))
-matrix = [list(alphabet), list(reversed_alphabet)]
+def input_message():
+    while(True):
+        encrypted_message = input("Enter encrypted message: ")
+        if encrypted_message.isalpha(): 
+            break
+        else:
+            print("Invalid input. Please enter a string without digits or symbols.")
+    return encrypted_message
 
-encrypted_message = "ovdrh xsvhhnvm"  # insert encrypted message here
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+upper_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+reversed_ua = ''.join(reversed(upper_alphabet))
+reversed_alphabet = ''.join(reversed(alphabet))
+matrix = [list(alphabet), list(upper_alphabet), list(reversed_alphabet), list(reversed_ua)]
+
+encrypted_message = input_message()
 decrypted_message = decrypt(encrypted_message, matrix)
 
 print("Decrypted message: ", decrypted_message)
